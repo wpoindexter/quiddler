e2b915f96461d20bf37dc0651fa60c0afa1cb0e9
William Poindexter <wryanp@Williams-MacBook-Pro.local>
2015-07-26 23:53:15 -0400

    rubocop

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,6 +1,6 @@
 module Api
   class BaseController < ActionController::Base
     protect_from_forgery with: :null_session
-    skip_before_filter  :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
   end
 end
